98fccddd3ea85b084fdb3cd3b58c56206fd4bf8f
Belén Huanca <beluh065@gmail.com>
2023-09-07 11:27:35 -0300

    Borré un post y un usuario

--- Modificación.sqlite/Ejercicio_3.sql
+++ Modificación.sqlite/Ejercicio_3.sql
@@ -2,7 +2,6 @@
 --USER
 UPDATE user SET username = "abrilArteaga"
 WHERE id = 1;
-
 --POST
 UPDATE post SET body = "2000"
 WHERE author_id >= 3;
---
+++ Modificación.sqlite/Ejercicio_4.sql
@@ -0,0 +1,6 @@
+--4) Delete de usuario y/o post
+--POST
+DELETE FROM post WHERE title = "3333";
+
+--USER
+DELETE FROM user WHERE username = "VeroP";
