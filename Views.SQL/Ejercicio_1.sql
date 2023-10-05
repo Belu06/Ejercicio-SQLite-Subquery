fe99b599480038b07a3158afc89b56bb4c37953c
Belén Huanca <beluh065@gmail.com>
2023-10-05 11:30:26 -0300

    3er ejercicio view

--- Views.SQL/Ejercicio_1.sql
+++ Views.SQL/Ejercicio_1.sql
@@ -1,6 +1,6 @@
 1. ¿Cuántas películas hay en cada tipo de "rating" (g, pg, pg13, r, nc17)
 
-CREATE VIEW peliculas_por_rating
+CREATE VIEW peliculas_por_rating AS
 SELECT COUNT(title) AS peliculas, rating
 	FROM "film"
 	GROUP BY rating;
--- Views.SQL/Ejercicio_2.sql
+++ Views.SQL/Ejercicio_2.sql
@@ -1,6 +1,6 @@
 2. ¿Cuántas películas hay en cada "categoría"?
 
-CREATE VIEW peliculas_por_categoria
+CREATE VIEW peliculas_por_categoria AS
 SELECT COUNT(f.title) AS peliculas, c.name AS categoria
 	FROM film f JOIN film_category fc
 	ON f.film_id = fc.film_id
