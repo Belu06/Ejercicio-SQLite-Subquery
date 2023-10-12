92a8be6eb82cbda8f5381bf50ea1bf4e26f11b23
Belén Huanca <beluh065@gmail.com>
2023-10-12 10:50:18 -0300

    4to Ejercicio view terminado

--- Views.SQL/Ejercicio_4.sql
+++ Views.SQL/Ejercicio_4.sql
@@ -1,12 +1,16 @@
 4. ¿Cuántos ejemplares (inventory) en total hay en cada local?  Como no hay nombre del local, poner en su lugar la dirección incluyendo ciudad y país además de la cantidad
 
 DROP VIEW direccion_completa;
-CREATE VIEW direccion_completa AS
-SELECT a.address, c.city, co.country
+CREATE VIEW direccion_completa AS           
+SELECT address_id,a.address AS direccion, c.city AS ciudad, co.country AS pais
 FROM address a JOIN city c
 ON a.city_id = c.city_id
 JOIN country co
 ON c.country_id = co.country_id;
 
-CREATE VIEW 
-SELECT inventory_id, 
+SELECT COUNT(film_id), dc.direccion, dc.ciudad, dc.pais
+FROM inventory i JOIN store s
+ON i.store_id = s.store_id
+JOIN direccion_completa dc
+ON s.address_id = dc.address_id
+GROUP BY dc.direccion, dc.ciudad, dc.pais
